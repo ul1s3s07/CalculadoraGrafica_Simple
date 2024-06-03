7d7e44b94a7e9557bd9a34ec660718b3c507b1b2
ul1s3s07 <apereiraa2010@gmail.com>
2024-06-03 08:26:56 -0300

    First Push

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+
+package calculadora;
+
+import gui.Pantalla;
+
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+       
+        Pantalla pant = new Pantalla();
+        pant.setVisible(true);
+        pant.setLocationRelativeTo(null);
+        
+    }
+    
+}
